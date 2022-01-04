f9c522f375e7973c962947ebebea9ca7c4a9c345
Emma <emmandersson166@gmail.com>
2022-01-04 11:03:16 +0100

    added home and signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+// Route imports
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App">hej</div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="signup" element={<Signup />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./contexts/AuthContext";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+render(
+	<BrowserRouter>
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
+	</BrowserRouter>,
+	document.getElementById("root")
+);
