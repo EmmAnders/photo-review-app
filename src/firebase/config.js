d2c546fd5c426947ea5ac8f4cf549f53e3aab6de
Emma <emmandersson166@gmail.com>
2022-01-04 10:18:36 +0100

    firebase  config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAwnhnPtRHbSap7EGCC-gEdJbT_tAGDThM",
+	authDomain: "photo-review-app-28678.firebaseapp.com",
+	projectId: "photo-review-app-28678",
+	storageBucket: "photo-review-app-28678.appspot.com",
+	messagingSenderId: "728275299809",
+	appId: "1:728275299809:web:40143618c73e30cadef6a0",
+};
+
+//Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get Firestore Instance
+const db = getFirestore(app);
+
+export { app as default, db };
