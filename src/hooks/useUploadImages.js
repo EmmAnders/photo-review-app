25ccac854f8b651c9cd002cf89d5168f99b658e4
Emma <emmandersson166@gmail.com>
2022-01-09 09:40:07 +0100

    created hook for uploading images

---
+++ src/hooks/useUploadImages.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useAuthContext } from "../contexts/AuthContext";
+
+//firebase imports
+import { db, storage } from "../firebase/config";
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
+const useUploadImages = () => {
+	const [error, setError] = useState(null);
+	const [isError, setIsError] = useState(null);
+	const [isUploading, setIsUploading] = useState(null);
+	const [isSuccess, setIsSuccess] = useState(null);
+	const [progress, setProgress] = useState(null);
+
+	const { user } = useAuthContext();
+
+	const upload = async (image, documentId) => {
+		setError(null);
+		setIsError(null);
+		setIsSuccess(null);
+		setIsUploading(true);
+		setProgress(null);
+
+		if (!image instanceof File) {
+			setError("Not accepted file");
+			setIsError(true);
+			setIsUploading(false);
+			return;
+		}
+
+		try {
+			const storageRef = ref(storage, `${user.uid}/${image.name}`);
+			const uploadTask = uploadBytesResumable(storageRef, image);
+
+			uploadTask.on("state_changed", (snapshot) => {
+				setProgress(
+					Math.round(
+						snapshot.bytesTransferred / snapshot.totalBytes
+					) * 100
+				);
+			});
+
+			await uploadTask.then();
+
+			const downloadUrl = await getDownloadURL(storageRef);
+			const documentRef = doc(db, "photoCollections", documentId);
+
+			await updateDoc(documentRef, {
+				images: arrayUnion({
+					name: image.name,
+					path: storageRef.fullPath,
+					size: image.size,
+					type: image.type,
+					url: downloadUrl,
+				}),
+			});
+			setProgress(null);
+			setIsSuccess(true);
+			setIsUploading(false);
+		} catch (e) {
+			console.log("ERROOOOOR", e.message);
+			setError(e.message);
+			setIsError(true);
+			setIsUploading(false);
+			setIsSuccess(false);
+		}
+	};
+
+	return { error, isError, isUploading, isSuccess, progress, upload };
+};
+
+export default useUploadImages;
