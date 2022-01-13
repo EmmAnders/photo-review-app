6e2978fd63a8c69a4222eb97f7dcefcbb1fda89d
Emma <emmandersson166@gmail.com>
2022-01-13 19:46:14 +0100

    created modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,18 +1,18 @@
 import "./Modal.scss";
 
-const Modal = (props) => {
+const Modal = ({ close, title, body, footer }) => {
 	return (
 		<>
-			<div onClick={props.close} className="modal-overlay"></div>
+			<div onClick={close} className="modal-overlay"></div>
 			<div className="modal">
 				<div className="modal-header">
-					<div>{props.title}</div>
-					<div className="modal-header-close" onClick={props.close}>
+					<div>{title}</div>
+					<div className="modal-header-close" onClick={close}>
 						Close
 					</div>
 				</div>
-				<div className="modal-body">{props.body}</div>
-				<div className="modal-footer">{props.footer}</div>
+				<div className="modal-body">{body}</div>
+				<div className="modal-footer">{footer}</div>
 			</div>
 		</>
 	);
